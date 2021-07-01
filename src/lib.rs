143e5c8835d04182b3e239e1dc6e5b32b07e870e
Grissess <grissess@nexusg.org>
2021-07-01 19:56:28 -0400

    Intial commit

---
+++ src/lib.rs
@@ -0,0 +1,327 @@
+use std::ptr::{self, NonNull};
+use std::marker::PhantomData;
+use std::cell::UnsafeCell;
+use std::rc::Rc;
+
+pub struct Visitor {
+    _not_pub_constructable: (),
+}
+
+pub trait Trace {
+    fn trace(&self, visitor: &Visitor);
+}
+
+pub trait Sealed {}
+
+pub trait Traverse: Sealed {
+    fn mark(&mut self);
+    fn unmark(&mut self);
+    fn marked(&self) -> bool;
+    fn next(&self) -> GcPtr;
+    fn prev(&self) -> GcPtr;
+}
+
+pub type GcPtr = *const dyn Traverse;
+pub type GcPtrNonNull = NonNull<dyn Traverse>;
+
+pub struct Arena {
+    start: GcPtr,
+    roots: Vec<GcPtrNonNull>,
+}
+
+pub struct ArenaIter<'a> {
+    cur: GcPtr,
+    // Mark as referring to the Arena, even though we just chase internal pointers.
+    // The &mut is because concurrent access is not yet safe.
+    marker: PhantomData<&'a mut Arena>,
+}
+
+// As with RcBox, repr(C) forces field order (to be sure that the layout is compatible with T:
+// ?Sized).
+#[repr(C)]
+pub struct GcBox<T: ?Sized> {
+    mark: bool,
+    next: GcPtr,
+    prev: GcPtr,
+    meta: *const (),  // vtable for &T as &dyn Trace
+    alloc: *mut GcAlloc<T>,  // point back to our alloc for dropping
+    value: T,
+}
+
+pub struct GcAlloc<T: ?Sized> {
+    inner: Option<NonNull<GcBox<T>>>,
+}
+
+pub struct Gc<T: ?Sized> {
+    ptr: Rc<UnsafeCell<GcAlloc<T>>>,
+    marker: PhantomData<GcBox<T>>,
+}
+
+pub struct Collection {
+    pub total: usize,
+    pub collected: usize,
+}
+
+fn null_gcptr() -> GcPtr {
+    unsafe {
+        // SAFETY: very little.
+        // The layout of this double matches the (unstable) core::raw::TraitObject. Beyond that,
+        // anything goes.
+        std::mem::transmute(
+            (ptr::null::<*const ()>(), ptr::null::<*const ()>())
+        )
+    }
+}
+
+fn extract_meta(t: &dyn Trace) -> *const () {
+    unsafe {
+        // SAFETY: as above.
+        // XXX Cast might be superfluous.
+        let (_ptr, meta): (*const (), *const ()) =
+            std::mem::transmute(t as *const dyn Trace);
+        meta
+    }
+}
+
+impl Arena {
+    pub fn new() -> Self {
+        Self {
+            start: null_gcptr(),
+            roots: Vec::new(),
+        }
+    }
+
+    pub fn gc<T: Trace + 'static>(&mut self, value: T) -> Gc<T> {
+        let gc = Gc::new(value);
+        unsafe {
+            // SAFETY: We're confident that this freshly-constructed Gc contains a unique, new
+            // allocation (by Box) to a GcBox.
+            let pt = (*gc.ptr.get()).inner.unwrap().as_ptr();
+            if let Some(gcbox) = (self.start as *mut GcBox<()>).as_mut() {
+                gcbox.prev = pt;
+            }
+            (*gc.ptr.get()).inner.unwrap().as_mut().next = self.start;
+            self.start = pt;
+        }
+        gc
+    }
+    
+    pub fn root<T: Trace + 'static>(&mut self, value: T) -> Gc<T> {
+        let gc = self.gc(value);
+        unsafe {
+            // SAFETY: Most of the worry here is just dereferencing the UnsafeCell. Since the
+            // interior type is Copy, this should be fine.
+            self.roots.push((*gc.ptr.get()).inner.unwrap());
+        }
+        gc
+    }
+
+    pub fn unroot<T>(&mut self, gc: &Gc<T>) {
+        // FIXME: This is expected to be a cold path
+        self.roots = self.roots.iter().cloned()
+            .filter(|ptr| !ptr::eq(ptr.as_ptr() as *const _, unsafe { *gc.ptr.get() }.inner.unwrap().as_ptr() as *const _))
+            .collect();
+    }
+
+    pub fn iter<'s>(&'s mut self) -> ArenaIter<'s> {
+        ArenaIter {
+            cur: self.start,
+            marker: PhantomData,
+        }
+    }
+
+    pub fn collect(&mut self) -> Collection {
+        let mut col = Collection {
+            total: 0, collected: 0,
+        };
+        for mut t in self.iter() {
+            unsafe {
+                // SAFETY: We expect these to have been already constructed and aligned normally,
+                // and this iterator--strictly speaking--returns only non-null pointers.
+                t.as_mut().unmark();
+            }
+            col.total += 1;
+        }
+        let visitor = Visitor {
+            _not_pub_constructable: (),
+        };
+        // Strictly speaking, we don't mutate the _values_ in this list, but we do mutate their
+        // referents through the underlying raw pointer.
+        for r in &mut self.roots {
+            unsafe {
+                // SAFETY: By virtue of this very line, the roots list cannot be left with dangling
+                // pointers (as all member objects are marked).
+                (*r).as_mut().mark();
+            }
+            // With that mut borrow out of scope, do the recursive trace
+            unsafe {
+                // SAFETY: Begin your dragon prayers.
+                // We've sealed Traverse as a trait, so we know our implementors (and it's only
+                // GcBox).
+                // This cast intentionally discards the Traverse vtable--we won't need it again.
+                // The type we chose for T is definitely wrong, but we make up for it by manually
+                // assembling the fat pointer.
+                let gcbox = r.cast::<GcBox<()>>().as_ref();
+                let tobj: &dyn Trace = std::mem::transmute(
+                    (&gcbox.value, gcbox.meta)
+                );
+                tobj.trace(&visitor);
+            }
+        }
+        let mut start = self.start;
+        for t in self.iter() {
+            if ! unsafe { t.as_ref().marked() } {
+                unsafe {
+                    let boxptr = t.cast::<GcBox<()>>();
+                    // TODO XXX: Unthread pointers in gcbox and change self.start if need be
+                    if ptr::eq(start, t.as_ptr()) {
+                        start = boxptr.as_ref().next;
+                    }
+                    {
+                        let boxmut = boxptr.as_ptr().as_mut().unwrap();
+                        if let Some(prevbox) = (boxmut.prev as *mut GcBox<()>).as_mut() {
+                            prevbox.next = boxmut.next;
+                        }
+                        if let Some(nextbox) = (boxmut.next as *mut GcBox<()>).as_mut() {
+                            nextbox.prev = boxmut.prev;
+                        }
+                    }
+                    // Null out the pointer to the box from its alloc, so all the Gc<T>'s pointing
+                    // here know that the allocation is gone.
+                    (*t.cast::<GcBox<()>>().as_mut().alloc).inner = None;
+                    // Collect the box again and let it drop
+                    Box::from_raw(t.as_ptr());
+                }
+                col.collected += 1;
+            }
+        }
+        self.start = start;
+        col
+    }
+}
+
+impl<'s> IntoIterator for &'s mut Arena {
+    type Item = GcPtrNonNull;
+    type IntoIter = ArenaIter<'s>;
+    fn into_iter(self) -> Self::IntoIter {
+        self.iter()
+    }
+}
+
+impl<T: ?Sized> Gc<T> {
+    pub fn as_ref(&self) -> Option<&T> {
+        unsafe {
+            // SAFETY: we're bounding the reference implicitly with the lifetime on self.
+            (*self.ptr.get()).inner.map(|pr| {
+                &pr.as_ref().value
+            })
+        }
+    }
+
+    pub fn as_mut(&mut self) -> Option<&mut T> {
+        unsafe {
+            // SAFETY: As above; note the mutable borrow of self to statically guarantee
+            // uniqueness.
+            (*self.ptr.get()).inner.map(|mut pr| {
+                &mut pr.as_mut().value
+            })
+        }
+    }
+}
+
+impl<T: Trace> Gc<T> {
+    fn new(value: T) -> Self {
+        let result = Self {
+            ptr: Rc::new(UnsafeCell::new(GcAlloc {
+                inner: Some(Box::leak(
+                         Box::new(GcBox {
+                             mark: false,
+                             next: null_gcptr(),
+                             prev: null_gcptr(),
+                             meta: extract_meta(&value as &dyn Trace),
+                             alloc: ptr::null_mut(),
+                             value
+                         })
+                    ).into()),
+                })),
+            marker: PhantomData,
+        };
+        unsafe {
+            // SAFETY: result.ptr is an UnsafeCell, so the compiler knows this can alias. In
+            // addtion, only one reference (a mutable one) is coined here, and dropped by this
+            // block--the provnenance doesn't include the *mut coming out of UnsafeCell::get().
+            (*result.ptr.get()).inner.unwrap().as_mut().alloc =
+                result.ptr.get();
+        }
+        result
+    }
+}
+
+impl<T> Clone for Gc<T> {
+    fn clone(&self) -> Self {
+        Self {
+            ptr: Rc::clone(&self.ptr),
+            marker: PhantomData,
+        }
+    }
+}
+
+impl<T: ?Sized> Sealed for GcBox<T> {}
+impl<T: ?Sized> Traverse for GcBox<T> {
+    fn mark(&mut self) { self.mark = true; }
+    fn unmark(&mut self) { self.mark = false; }
+    fn marked(&self) -> bool { self.mark }
+    fn next(&self) -> GcPtr { self.next }
+    fn prev(&self) -> GcPtr { self.prev }
+}
+
+impl<'a> Iterator for ArenaIter<'a> {
+    type Item = GcPtrNonNull;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        unsafe {
+            // SAFETY: Pray to the dragons that we've maintained a valid linked list elsewhere
+            self.cur.as_ref()
+        }.map(|t| {
+            self.cur = t.next();
+            unsafe {
+                // SAFETY: This code is reachable only when the pointer isn't null
+                GcPtrNonNull::new_unchecked(t as GcPtr as *mut _)
+            }
+        })
+    }
+}
+
+// Manual impls to avoid constraints on the underlying T
+impl<T: ?Sized> Clone for GcAlloc<T> {
+    fn clone(&self) -> Self {
+        *self
+    }
+}
+
+impl<T: ?Sized> Copy for GcAlloc<T> {}
+
+impl Visitor {
+    pub fn visit<T>(&self, gc: &Gc<T>) {
+        let gcbox = unsafe {
+            // SAFETY: Rely on this being constructed and not dropped.
+            // Aliasing: Rust would normally complain about seeking a &mut from our &Gc, but the
+            // underlying raw pointer is represented as mutable.
+            (*gc.ptr.get()).inner.unwrap().as_mut()
+        };
+        if gcbox.mark {
+            return;
+        }
+        gcbox.mark = true;
+        unsafe {
+            // SAFETY: See Arena::collect.
+            let tobj: &dyn Trace = std::mem::transmute(
+                (&gcbox.value, gcbox.meta)
+            );
+            tobj.trace(&self);
+        }
+    }
+}
+
+#[cfg(test)]
+mod test;
---
+++ src/test.rs
@@ -0,0 +1,211 @@
+use super::*;
+
+use std::fmt::{self, Debug, Formatter};
+
+// XXX don't try this at home; I'm doing this because (in the tests) I don't feel like introducing
+// lifetime parameters.
+pub struct RunOnDrop {
+    func: fn(*mut ()),
+    data: *mut (),
+}
+
+impl Drop for RunOnDrop {
+    fn drop(&mut self) {
+        (self.func)(self.data);
+    }
+}
+
+enum Object {
+    Simple,
+    Container(Vec<Gc<Object>>),
+    RunOnDrop(RunOnDrop),
+}
+
+// This implementation is intentionally minimal for the uses of these test cases.
+impl PartialEq for Object {
+    fn eq(&self, other: &Self) -> bool {
+        use Object::*;
+        match (self, other) {
+            (Simple, Simple) => true,
+            _ => false,
+        }
+    }
+}
+
+impl Eq for Object {}
+
+impl Debug for Object {
+    fn fmt(&self, f: &mut Formatter) -> fmt::Result {
+        use Object::*;
+        match self {
+            Simple => write!(f, "Simple"),
+            Container(v) => write!(f, "Container:{}", v.len()),
+            RunOnDrop(_) => write!(f, "RunOnDrop:?"),
+        }
+    }
+}
+
+impl Trace for Object {
+    fn trace(&self, visitor: &Visitor) {
+        match self {
+            Object::Simple => (),
+            Object::Container(ref v) => {
+                for r in v {
+                    visitor.visit(r);
+                }
+            },
+            Object::RunOnDrop(_) => (),
+        }
+    }
+}
+
+#[test]
+fn doesnt_free_roots() {
+    let mut arena = Arena::new();
+    let a = arena.root(Object::Simple);
+    let b = arena.root(Object::Simple);
+    let col = arena.collect();
+    assert_eq!(col.total, 2);
+    assert_eq!(col.collected, 0);
+}
+
+#[test]
+fn frees_unrooted() {
+    let mut arena = Arena::new();
+    let a = arena.gc(Object::Simple);
+    let b = arena.gc(Object::Simple);
+    let col = arena.collect();
+    assert_eq!(col.total, 2);
+    assert_eq!(col.collected, 2);
+}
+
+#[test]
+fn visits_children() {
+    let mut arena = Arena::new();
+    let c = arena.gc(Object::Simple);
+    let b = arena.gc(Object::Simple);
+    let a = arena.root(Object::Container(vec![c]));
+    let col = arena.collect();
+    assert_eq!(col.total, 3);
+    assert_eq!(col.collected, 1);
+}
+
+#[test]
+fn calls_drop() {
+    let mut arena = Arena::new();
+    let mut drop_cnt = 0usize;
+    
+    fn _increment(i: *mut ()) {
+        unsafe {
+            // SAFETY: this is a pointer to drop_cnt in the scope above; it'd be safer if this were
+            // a reference, and it only isn't because of laziness.
+            *(i as *mut usize) += 1;
+        }
+    }
+
+    let a = arena.gc(Object::RunOnDrop(
+            RunOnDrop {
+                func: _increment,
+                data: &mut drop_cnt as *mut _ as *mut (),
+            }
+    ));
+    let col = arena.collect();
+    assert_eq!(col.total, 1);
+    assert_eq!(col.collected, 1);
+    assert_eq!(drop_cnt, 1);
+}
+
+#[test]
+fn rooted_borrow_lives() {
+    let mut arena = Arena::new();
+    let mut a = arena.root(Object::Simple);
+    let col = arena.collect();
+    assert_eq!(col.total, 1);
+    assert_eq!(col.collected, 0);
+    assert_eq!(a.as_ref(), Some(&Object::Simple));
+    assert_eq!(a.as_mut(), Some(&mut Object::Simple));
+}
+
+#[test]
+fn unrooted_borrow_dies() {
+    let mut arena = Arena::new();
+    let mut a = arena.gc(Object::Simple);
+    let col = arena.collect();
+    assert_eq!(col.total, 1);
+    assert_eq!(col.collected, 1);
+    assert_eq!(a.as_ref(), None);
+    assert_eq!(a.as_mut(), None);
+}
+
+#[test]
+fn borrow_dies_after_unroot() {
+    let mut arena = Arena::new();
+    let mut a = arena.root(Object::Simple);
+    let col = arena.collect();
+    assert_eq!(col.total, 1);
+    assert_eq!(col.collected, 0);
+    assert_eq!(a.as_ref(), Some(&Object::Simple));
+    assert_eq!(a.as_mut(), Some(&mut Object::Simple));
+    arena.unroot(&a);
+    let col = arena.collect();
+    assert_eq!(col.total, 1);
+    assert_eq!(col.collected, 1);
+    assert_eq!(a.as_ref(), None);
+    assert_eq!(a.as_mut(), None);
+}
+
+#[test]
+fn iterative() {
+    let mut arena = Arena::new();
+    for i in 0..5 {
+        let mut a = arena.root(Object::Simple);
+        let col = arena.collect();
+        assert_eq!(col.total, 1);
+        assert_eq!(col.collected, 0);
+        assert_eq!(a.as_ref(), Some(&Object::Simple));
+        assert_eq!(a.as_mut(), Some(&mut Object::Simple));
+        arena.unroot(&a);
+        let col = arena.collect();
+        assert_eq!(col.total, 1);
+        assert_eq!(col.collected, 1);
+        assert_eq!(a.as_ref(), None);
+        assert_eq!(a.as_mut(), None);
+    }
+}
+
+#[test]
+fn no_boxes_in_empty_arena() {
+    let mut arena = Arena::new();
+    assert_eq!(arena.iter().next(), None);
+}
+
+#[test]
+fn no_boxes_in_reclaimed_arena() {
+    let mut arena = Arena::new();
+    for count in 1..5 {
+        let refs = std::iter::repeat_with(|| {
+            arena.gc(Object::Simple)
+        }).take(count).collect::<Vec<_>>();
+        let col = arena.collect();
+        assert_eq!(col.total, count);
+        assert_eq!(col.collected, count);
+        assert_eq!(arena.iter().next(), None);
+    }
+}
+
+#[test]
+fn roots_outlive_refs() {
+    let mut arena = Arena::new();
+    let mut total = 0usize;
+    for count in 1..5 {
+        let refs = std::iter::repeat_with(|| {
+            arena.root(Object::Simple)
+        }).take(count).collect::<Vec<_>>();
+        total += count;
+        let col = arena.collect();
+        assert_eq!(col.total, total);
+        assert_eq!(col.collected, 0);
+        assert!(arena.iter().next().is_some());
+        assert_eq!(arena.iter().count(), total);
+    }
+}
